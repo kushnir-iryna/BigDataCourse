4a9a824b35db28d168c058d3e3042d10fbeda56c
kushnir-iryna <30667969+kushnir-iryna@users.noreply.github.com>
2017-08-06 21:34:56 +0300

    changing into English

--- Fibonacci/src/ua/com/ikcorp/Fibonacci.java
+++ Fibonacci/src/ua/com/ikcorp/Fibonacci.java
@@ -14,7 +14,7 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		double p = 0;
-		System.out.println("Введіть кількість елементів");
+		System.out.println("Enter number of Fibonacci numbers: ");
 		Scanner s = new Scanner(System.in);
 		int k = s.nextInt();
 		for (int i = 0; i < k; i++) {
@@ -24,24 +24,24 @@ public class Fibonacci {
 			}
 		}
 		System.out.println();
-		System.out.print("Кількість парних чисел рівна ");
+		System.out.print("Quantity of even numbers: ");
 		System.out.printf("%.0f", p);
 		double a = (p * 100 / k);
-		System.out.print(". У відсотковому співвідношені це рівне ");
+		System.out.print(". In percentages that's equal ");
 		System.out.printf("%.2f", a);
 		System.out.println("%");
 		System.out.println();
-		System.out.print("Кількість непарних чисел рівна ");
+		System.out.print("Quantity of odd numbers: ");
 		System.out.printf("%.0f", (k - p));
 		double b = ((k - p) * 100 / k);
-		System.out.print(". У відсотковому співвідношені це рівне ");
+		System.out.print(". In percentages that's equal ");
 		System.out.printf("%.2f", b);
 		System.out.println("%");
 		System.out.println();
-		System.out.println("Введіть номер елемента");
+		System.out.println("Enter sequence number of Fibonacci number: ");
 		int m = s.nextInt();
 		// if ((m <= k) & (m > 0)) {
-		System.out.println("Число Фібоначі під номером " + m + " рівне " + fibonac(m - 1));
+		System.out.println("Fibonacci numbe " + m + " is equal " + fibonac(m - 1));
 		// } else
 		// System.out.println("ПЕРЕВИЩЕНА КІЛЬКІСТЬ ЕЛЕМЕНТІВ");
 
